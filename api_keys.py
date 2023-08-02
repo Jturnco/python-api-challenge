87e0de7a0f8d959dea02a75450d6b4764453edf2
Jturnco <jturnco@gmail.com>
2023-08-02 15:48:54 -0700

    Updates

---
+++ api_keys.py
@@ -0,0 +1,9 @@
+# Adding config.py file.
+api_keys.py
+
+# OpenWeatherMap API Key
+weather_api_key = "1317e3945dd049d6bef3a88ee33dfe39"
+
+# Geoapify API Key
+geoapify_key = "c0701fb3ca2f41af9d521cab5db2e671"
+
